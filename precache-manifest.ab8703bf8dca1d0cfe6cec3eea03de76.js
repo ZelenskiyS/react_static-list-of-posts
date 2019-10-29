896a93f033488d9d41fc279cd2517f40c0e5d899
Simon <semen.zelenskiy@gmail.com>
2019-10-29 22:57:40 +0200

    Updates

---
+++ precache-manifest.ab8703bf8dca1d0cfe6cec3eea03de76.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "256f4ffc909050cf33a83fd92055e01e",
+    "url": "/react_static-list-of-posts/index.html"
+  },
+  {
+    "revision": "c2081fb06373effc4de6",
+    "url": "/react_static-list-of-posts/static/css/main.0b30318e.chunk.css"
+  },
+  {
+    "revision": "26050395c0748c903b05",
+    "url": "/react_static-list-of-posts/static/js/2.67a0c2f2.chunk.js"
+  },
+  {
+    "revision": "c2081fb06373effc4de6",
+    "url": "/react_static-list-of-posts/static/js/main.2f99d13d.chunk.js"
+  },
+  {
+    "revision": "3fdea9cdc8e2a1e9a77e",
+    "url": "/react_static-list-of-posts/static/js/runtime~main.c48bb6e1.js"
+  }
+]);
